93417ee5fabaf44d16c8ad372a643440500e82a7
Donny Landis <donny.landis@gmail.com>
2020-02-11 18:56:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -101,7 +101,7 @@ def changing_alan
      }
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for][0] = alans_new_info
-     programmer_hash[:alan_kay][:known_for]
+     programmer_hash
 end
 
 def adding_to_dennis
